eec078e8c5920e0083da068f6bb661cdba566095
Abbas <ebonebaoo928@gmail.com>
2026-08-04 18:03:27 +0500

    Virgin upgraded v1.0.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,9 +42,9 @@ android {
 
         targetSdk = 34
 
-        versionCode = 9
+        versionCode = 10
 
-        versionName = "1.0.9"
+        versionName = "1.0.10"
 
         testInstrumentationRunner =
             "androidx.test.runner.AndroidJUnitRunner"
